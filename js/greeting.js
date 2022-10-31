302433a80a57bf62510798000fcd0a6464f3f2f0
YoonJung Shin <101538757+s-yoonj@users.noreply.github.com>
2022-10-31 13:09:16 +0900

    Add files via upload

--- js/greeting.js
+++ js/greeting.js
@@ -1,29 +1,31 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+const todoForm = document.querySelector('#todo-form')
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit(e) {
-  e.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+    e.preventDefault()
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username = loginInput.value
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username)
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `안녕하세요, ${username}님. 반가워요!`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `안녕하세요, ${username}님. 반가워요!`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+    todoForm.classList.remove(HIDDEN_CLASSNAME)
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
+const savedUsername = localStorage.getItem(USERNAME_KEY)
 // 로컬 스토리지 확인이 첫번째 시작
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener('submit', onLoginSubmit)
 } else {
-  paintGreetings(savedUsername);
+    paintGreetings(savedUsername)
 }
